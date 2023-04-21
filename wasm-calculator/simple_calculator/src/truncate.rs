d35f4bdf4e2178af03e8849291db4cf15a1a39f2
Alex Gourlay <alex.gourlay-cic@ibm.com>
2023-04-21 14:31:51 +0100

    Update truncate docs.

--- wasm-calculator/simple_calculator/src/truncate.rs
+++ wasm-calculator/simple_calculator/src/truncate.rs
@@ -1,19 +1,22 @@
+/// For truncating things.
 pub trait Truncate {
     fn truncate_nums(&self, len: usize) -> &str;
 }
 
 impl Truncate for str {
+    /// Truncate the number of numeric characters to a given maximum count.
+    ///
     // TODO: round last digit.
     fn truncate_nums(&self, max_count: usize) -> &str {
-        let mut num_count = 0;
+        let mut numeric_char_count = 0;
         let mut slice_index = 0;
 
-        for (i, char) in self.chars().enumerate() {
-            slice_index = i;
+        for (index, char) in self.chars().enumerate() {
+            slice_index = index;
             if char.is_digit(10) {
-                num_count += 1;
+                numeric_char_count += 1;
             }
-            if num_count == max_count {
+            if numeric_char_count == max_count {
                 break;
             }
         }
@@ -27,13 +30,27 @@ mod tests {
 
     #[test]
     fn truncate_nums() {
+        let test_str = "1234";
+        assert_eq!(
+            test_str.truncate_nums(2),
+            "12",
+            "Should reduce the number of numeric characters to the given count."
+        );
+    }
+
+    #[test]
+    fn truncate_nums_decimal() {
         let test_str = "1.234";
-        assert_eq!(test_str.truncate_nums(2), "1.2");
+        assert_eq!(
+            test_str.truncate_nums(2),
+            "1.2",
+            "Should reduce the number of numeric characters to the given count."
+        );
     }
 
     #[test]
     fn truncate_nums_over_len() {
-        let test_str = "1.234";
-        assert_eq!(test_str.truncate_nums(6), "1.234");
+        let test_str = "1234";
+        assert_eq!(test_str.truncate_nums(6), test_str, "Should have no effect when the max count is greater than the number of numeric characters.");
     }
 }
